0d53e714434cdf24cc687199fa564d306a980830
Simon Walker <github@helpmebot.org.uk>
2011-05-30 22:57:36 +0100

    layout of interface

--- gen/com/stwalkerster/android/apps/strobelight/R.java
+++ gen/com/stwalkerster/android/apps/strobelight/R.java
@@ -13,11 +13,19 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int SeekBar01=0x7f050001;
+        public static final int TextView01=0x7f050000;
+        public static final int TextView02=0x7f050003;
+        public static final int ToggleButton01=0x7f050002;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f040000;
+        public static final int healthwarning=0x7f040003;
+        public static final int powerbutton=0x7f040001;
+        public static final int speed=0x7f040002;
     }
 }
