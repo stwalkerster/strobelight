a10e674fdd35d36cf6e02c4581ec01272a6f6c28
Simon Walker <github@helpmebot.org.uk>
2011-05-30 23:58:37 +0100

    turn on flashlight

--- src/com/stwalkerster/android/apps/strobelight/StrobeLightConfig.java
+++ src/com/stwalkerster/android/apps/strobelight/StrobeLightConfig.java
@@ -1,13 +1,29 @@
 package com.stwalkerster.android.apps.strobelight;
 
 import android.app.Activity;
+import android.hardware.Camera;
 import android.os.Bundle;
 
 public class StrobeLightConfig extends Activity {
+	
+	Camera cam;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        cam = Camera.open();
+        if(cam==null)
+        {
+        	//no camera;
+        }
+        else
+        {
+        	Camera.Parameters p = cam.getParameters();
+        	p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+        	cam.setParameters(p);
+        }
     }
 }
