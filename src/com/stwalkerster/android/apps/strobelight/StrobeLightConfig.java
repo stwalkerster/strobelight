575fbb0f94514d54cfd3d529ec3b34e7cda198ea
Simon Walker <github@helpmebot.org.uk>
2011-05-30 21:32:17 +0100

    initial commit

---
+++ src/com/stwalkerster/android/apps/strobelight/StrobeLightConfig.java
@@ -0,0 +1,13 @@
+package com.stwalkerster.android.apps.strobelight;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StrobeLightConfig extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
