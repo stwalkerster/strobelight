f70b58c4a831cc9c573937413042c822e1db2193
Simon Walker <github@stwalkerster.co.uk>
2012-11-20 00:14:34 +0000

    update for new SDK

---
+++ gen/com/stwalkerster/android/apps/strobelight/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.stwalkerster.android.apps.strobelight;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
