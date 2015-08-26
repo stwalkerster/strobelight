2cc0d706739d028a90ca7b47f07bd5a0a4ebc8d3
souch <rodolphe.souchaud@free.fr>
2015-08-26 13:42:18 +0200

    Show corresponding milliseconds.

--- src/com/stwalkerster/android/apps/strobelight/StrobeLightConfig.java
+++ src/com/stwalkerster/android/apps/strobelight/StrobeLightConfig.java
@@ -15,7 +15,9 @@ public class StrobeLightConfig extends Activity {
 	Camera cam;
 	StrobeRunner runner;
 	Thread bw;
-	
+	private TextView textViewOn;
+	private TextView textViewOff;
+
 	public final Handler mHandler = new Handler();
 	
     public final Runnable mShowToastRunnable = new Runnable() {
@@ -30,8 +32,10 @@ public class StrobeLightConfig extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        final ToggleButton togglebutton = (ToggleButton) findViewById(R.id.ToggleButton01);
-        
+        final ToggleButton togglebutton = (ToggleButton) findViewById(R.id.ToggleButtonStrobe);
+		textViewOn = (TextView) findViewById(R.id.TextViewOn);
+		textViewOff = (TextView) findViewById(R.id.TextViewOff);
+
         runner = StrobeRunner.getInstance();
         runner.controller = this;
         
@@ -49,7 +53,7 @@ public class StrobeLightConfig extends Activity {
 		        if(cam==null)
 		        {
 		        	togglebutton.setEnabled(false);
-		        	TextView t = (TextView)findViewById(R.id.TextView01);
+		        	TextView t = (TextView)findViewById(R.id.TextViewOn);
 		        	t.setText(R.string.nocamera);
 		        	return;
 		        }
@@ -59,7 +63,7 @@ public class StrobeLightConfig extends Activity {
         	catch(RuntimeException ex)
         	{
 	        	togglebutton.setEnabled(false);
-	        	TextView t = (TextView)findViewById(R.id.TextView01);
+	        	TextView t = (TextView)findViewById(R.id.TextViewOn);
 	        	t.setText(R.string.nocamera);
 	        	Toast.makeText(getApplicationContext(), "Error connecting to camera flash.", Toast.LENGTH_LONG).show();
 	        	return;
@@ -68,41 +72,42 @@ public class StrobeLightConfig extends Activity {
         
         
         togglebutton.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on clicks
-                if (togglebutton.isChecked()) {
-                    bw = new Thread(runner);
-                    bw.start();
-                } else {
-                    runner.requestStop = true;
-                }
-            }
-        });
-        
-        final SeekBar skbar = (SeekBar)findViewById(R.id.SeekBar01);
-        skbar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
-			
-			@Override
-			public void onStopTrackingTouch(SeekBar seekBar) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void onStartTrackingTouch(SeekBar seekBar) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void onProgressChanged(SeekBar seekBar, int progress,
-					boolean fromUser) {
-				runner.delay=progress;
-				
+			public void onClick(View v) {
+				// Perform action on clicks
+				if (togglebutton.isChecked()) {
+					bw = new Thread(runner);
+					bw.start();
+				} else {
+					runner.requestStop = true;
+				}
 			}
 		});
         
-        final SeekBar skbaroff = (SeekBar)findViewById(R.id.SeekBar02);
+        final SeekBar skbar = (SeekBar)findViewById(R.id.SeekBarOn);
+        skbar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress,
+										  boolean fromUser) {
+				runner.delay = progress;
+				textViewOn.setText(getResources().getString(R.string.speed) + " (" + progress + " ms)");
+			}
+		});
+		skbar.setProgress(runner.delay);
+        
+        final SeekBar skbaroff = (SeekBar)findViewById(R.id.SeekBarOff);
         skbaroff.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
 			
 			@Override
@@ -121,9 +126,10 @@ public class StrobeLightConfig extends Activity {
 			public void onProgressChanged(SeekBar seekBar, int progress,
 					boolean fromUser) {
 				runner.delayoff=progress;
-				
+				textViewOff.setText(getResources().getString(R.string.speedoff) + " (" + progress + " ms)");
 			}
 		});
+		skbaroff.setProgress(runner.delayoff);
 
         
     }
@@ -131,7 +137,7 @@ public class StrobeLightConfig extends Activity {
     @Override
     protected void onStop() {
     	runner.requestStop=true;
-        ToggleButton togglebutton = (ToggleButton) findViewById(R.id.ToggleButton01);
+        ToggleButton togglebutton = (ToggleButton) findViewById(R.id.ToggleButtonStrobe);
         togglebutton.setChecked(false);
     	
     	super.onStop();
@@ -150,7 +156,7 @@ public class StrobeLightConfig extends Activity {
 	    	toast.show();
     	}
     	
-        ToggleButton togglebutton = (ToggleButton) findViewById(R.id.ToggleButton01);
+        ToggleButton togglebutton = (ToggleButton) findViewById(R.id.ToggleButtonStrobe);
         togglebutton.setChecked(false);
     }
 }
